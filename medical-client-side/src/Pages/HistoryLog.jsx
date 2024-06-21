c80904361086ec07398e9a121939cc57e52bf57a
Mehboob Alam(A.D) <mehboobalam@gmail.com>
2024-06-20 22:09:44 -0700

    add routes in client side

---
+++ medical-client-side/src/Pages/HistoryLog.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HistoryLog() {
+  return (
+    <div>HistoryLog</div>
+  )
+}
+
+export default HistoryLog
---
+++ medical-client-side/src/routes/route.jsx
@@ -0,0 +1,30 @@
+import { createBrowserRouter } from 'react-router-dom'
+import App from '../App'
+import Store from '../Pages/Store'
+import HistoryLog from '../Pages/HistoryLog'
+import Dashboard from '../Pages/Dashboard'
+
+
+const routes = createBrowserRouter({
+    path: '/',
+    element: <App />,
+    children: [
+        {
+            path: '',
+            element: <Dashboard />,
+            children: [
+                {
+                    path: 'store',
+                    element: <Store />
+                },
+                {
+                    path: 'log',
+                    element: <HistoryLog />
+                }
+            ]
+        }
+    ]
+})
+
+
+export default routes
